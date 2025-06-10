77d343f4603d6d41deb6f7bbcb0661cb13a2cabe
daciodf <74731330+daciodf@users.noreply.github.com>
2025-06-10 10:18:34 -0300

    Create generate-ssl.sh

---
+++ generate-ssl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ ! -f "${SSL_CERT}" ] || [ ! -f "${SSL_KEY}" ]; then
+    echo "Generating self-signed certificate..."
+    mkdir -p $(dirname ${SSL_CERT}) $(dirname ${SSL_KEY})
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+        -keyout ${SSL_KEY} -out ${SSL_CERT} \
+        -subj "/C=US/ST=California/L=San Francisco/O=WebDAV/CN=webdav.local"
+fi
